ff1c077a178047c12fc563bd3331f8900c6ebe36
a_soloviev <2595700@gmail.com>
2024-10-23 15:34:02 +0300

    Was added time sleep

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 
 
@@ -5,6 +6,7 @@ def test_basket_button(browser):
 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(30)
     button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert len(button) > 0, "Кнопка Добавить в корзину не найдена"
 
