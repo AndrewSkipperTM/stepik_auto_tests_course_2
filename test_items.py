6399be161caa359880ea3fc05c338d07a8008b64
a_soloviev <2595700@gmail.com>
2024-10-23 14:55:04 +0300

    All files of project

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+def test_basket_button(browser):
+
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert len(button) > 0, "Кнопка Добавить в корзину не найдена"
+
